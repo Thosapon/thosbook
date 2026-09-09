4b4ca32a83991582a33e652ad96fbeab82cc094b
Thosapon <thosmuenchan@gmail.com>
2026-09-09 07:52:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Configuration for Thosbook Web Application
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxNg0j4XzNIb2mvYboa1Rrt8x-4SNhKhzGXVxQNuQ0c4R26bz3sdJCluwx9SZA1qY4c/exec";
